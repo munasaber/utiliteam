1c097980b3f43dfd7623872f669800371cee27c4
Muna Saber <munasaber@ucsb.edu>
2020-06-16 12:54:12 -0700

    Trying to make example working one but not working

--- projects/intersitials/runscript.sh
+++ projects/intersitials/runscript.sh
@@ -1,3 +1,4 @@
 #g++ interstitials.cpp xtal_classes.cpp symmetry_operations.cpp tests.cpp -o run
 g++ --std=c++17 ../avdv-factor-group/lattice.cpp ../avdv-factor-group/site.cpp ../avdv-factor-group/coordinate.cpp ../avdv-factor-group/symop.cpp ../avdv-factor-group/structure.cpp ../avdv-factor-group/tests.cpp interstitial_mesh.cpp interstitial_mesh_test.cpp -o interstitial_test
 exit 0
+g++ --std=c++17 ../avdv-factor-group/lattice.cpp ../avdv-factor-group/site.cpp ../avdv-factor-group/coordinate.cpp ../avdv-factor-group/symop.cpp ../avdv-factor-group/structure.cpp write_to_poscar.cpp interstitial_mesh.cpp main.cpp -o run_main
